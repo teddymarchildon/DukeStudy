78a1a611643aebd8a52977d6b4d57518d2cc003e
tmarchildon <tmm61@duke.edu>
2018-10-10 11:40:00 -0400

    added basic react page for testing

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 export default () => (
   <div>
-    <h1> Gray, Adam, Daniel, Teddy, Mike </h1>
+    <h1> Welcome to DukeStudy</h1>
+    <Link href='/tic-tac-toe'>
+    Click Here to play Tic Tac Toe
+    </Link>
   </div>
 )

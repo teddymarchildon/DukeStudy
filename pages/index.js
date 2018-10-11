25e438e314be2e945559a6792715430bb6cb1816
tmarchildon <tmm61@duke.edu>
2018-10-10 22:47:45 -0400

    updated file structure

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,52 @@
 import Link from 'next/link'
 import React, { Component } from "react"
 import { withRouter } from 'next/router'
+import QueryDatabase from '../database/connect'
 
 class Header extends React.Component {
   render() {
     return (
-    <div>
       <div>
-        <h1> Welcome to DukeStudy, {this.props.netid}</h1>
+        <div>
+          <h1> Welcome to DukeStudy, {this.props.netid}</h1>
+          <h1> You major in {this.props.major} </h1>
+        </div>
+        <div>
+          <Link href="/login">
+            <a>Logout</a>
+          </Link>
+        </div>
       </div>
-
-      <div>
-        <Link href="/login">
-          <a>Logout</a>
-        </Link>
-      </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default class HomePage extends React.Component {
 
   static async getInitialProps({ query }) {
-    return { netid: query.netid }
+
+    // let queryString = "SELECT Primary_Major from Student where NetID = " + query.netid;
+
+    // let results = QueryDatabase(queryString)
+
+    // const { Client } = require('pg')
+    // const client = new Client()
+    //
+    // await client.connect()
+    //
+    // const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    // console.log(res.rows[0].message) // Hello world!
+    // await client.end()
+
+    return {
+      netid: query.netid,
+      major: 'CompSci'
+    }
   }
 
   render() {
     return (
-        <Header netid={this.props.netid}/>
+        <Header netid={this.props.netid} major={this.props.major}/>
     );
   }
 }

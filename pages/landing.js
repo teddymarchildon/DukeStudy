8c52ab7a74a5762cde43ba109a0d5924e1706103
tmarchildon <tmm61@duke.edu>
2018-11-07 10:50:10 -0500

    added full properties in landing and styling

--- pages/landing.js
+++ pages/landing.js
@@ -3,6 +3,31 @@ import React, { Component } from "react"
 import { withRouter } from 'next/router'
 import fetch from 'node-fetch';
 import SearchAppBar from '../components/app_bar.js'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    minWidth: 100,
+    maxWidth: 200
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
 
 class Header extends React.Component {
   render() {
@@ -11,13 +36,47 @@ class Header extends React.Component {
         <div>
           <header>
             <p> Welcome to DukeStudy, {this.props.name} </p>
-            <p> You major in {this.props.major} </p>
           </header>
         </div>
       </div>
     );
   }
 }
+
+class Content extends React.Component {
+  render() {
+    return (
+      <div style={{width: '50%', margin: '0 auto'}}>
+        <Card className={this.props.card}>
+          <CardContent>
+            <Typography className={this.props.title} color="textSecondary" gutterBottom>
+              Major: {this.props.major}
+            </Typography>
+            <Typography className={this.props.title} color="textSecondary" gutterBottom>
+              Minor: {this.props.minor}
+            </Typography>
+            <Typography className={this.props.title} color="textSecondary" gutterBottom>
+              Certificate: {this.props.certificate}
+            </Typography>
+            <Typography className={this.props.title} color="textSecondary" gutterBottom>
+              Favorite Class: {this.props.favClass}
+            </Typography>
+            <Typography className={this.props.title} color="textSecondary" gutterBottom>
+              Favorite Prof: {this.props.favProf}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button href = "/edit" size="small"> Edit </Button>
+          </CardActions>
+        </Card>
+      </div>
+    )
+  }
+}
+
+Content.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 export default class HomePage extends React.Component {
 
@@ -31,7 +90,15 @@ export default class HomePage extends React.Component {
     return (
       <div>
         <SearchAppBar> </SearchAppBar>
-        <Header name={this.props.name} major={this.props.primary_major}/>
+        <Header name={this.props.name}/>
+        <Content
+          major={this.props.primary_major}
+          favClass={this.props.favorite_class}
+          favProf={this.props.favorite_professor}
+          major={this.props.primary_major}
+          minor={this.props.primary_minor}
+          certificate={this.props.certificate}
+        />
       </div>
     );
   }

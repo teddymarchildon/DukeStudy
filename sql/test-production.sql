889f23fd70108f52e3b7209dc8a12ec58edd5182
gperrone5 <44028793+gperrone5@users.noreply.github.com>
2018-11-08 22:22:53 -0500

    Update test-production.sql

--- sql/test-production.sql
+++ sql/test-production.sql
@@ -1,6 +1,6 @@
--- 10 courses that a given professor is teaching 
+--all the courses that the professor is teaching this semester and when that class meets 
 select Course_Number
-from Teaches_Course
+from Course 
 where Professor_Name = 'Jun Yang'
 group by Course_Number
 limit 10;
@@ -13,19 +13,12 @@ where Favorite_Professor = 'Jun Yang'
 limit 10;
 
 
-
--- 10 students that have taken this course (random)
-select NetID, Course_Number
-from Takes_Course 
-where Course_Number = '82'
-group by NetID, Course_Number
-limit 10;
-
---classes taught by your favorite professor, check 
-select t.Professor_Name, t.Course_Number 
-from Student s, Teaches_Course t 
-where NetID = 'aa111'
-and s.Favorite_Professor = t.Professor_Name
+-- tutors that are avaliable on a given day 
+select tt.NetID, Days_Available, Rate_Per_Hour
+from Tutor tt 
+and Course_Number = '12572'
+and Days_Available like '%F%'
+and Rate_Per_Hour < 15
 limit 10;
 
 -- all the TAs that teach for a certain course - check 
@@ -34,8 +27,8 @@ from TAs_Course
 where Course_Number = '82'
 limit 10;
 
--- all the classes that satisfy the pre-rep ALP 
+-- all the TAs that teach for a certain course - check 
 select Course_Number
-from Course 
+from Course  
 where Prerequesites like '%ALP%'
 limit 10;

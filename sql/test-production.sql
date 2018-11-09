f7d8e23ec179f209841278191adf35dc4427c664
gperrone5 <44028793+gperrone5@users.noreply.github.com>
2018-11-08 19:16:18 -0500

    Update test-production.sql

--- sql/test-production.sql
+++ sql/test-production.sql
@@ -1,26 +1,26 @@
--- easiest classes at Duke, check
-select Course_Number, avg(Difficulty) as avgDifficulty
-from Rates_Course 
-group by Course_Number 
-order by avg(Difficulty) asc 
+--all the courses that the professor is teaching this semester - check! 
+select Course_Number
+from Teaches_Course 
+where Professor_Name = 'Jun Yang'
+and Year_Semester = 'Fall 2018'
+group by Course_Number
 limit 10;
 
--- classes with the least work at Duke, check 
-select Course_Number, avg(WorkLoad) as avgWorkload
-from Rates_Course 
-group by Course_Number 
-order by avg(WorkLoad) asc 
+-- students that have voted him their favorite professor - check! 
+
+select distinct NetID
+from Student 
+where Favorite_Professor = 'Jun Yang'
 limit 10;
 
--- classes with the least work at Duke in your major, need to add the XXX, check 
-select c.Course_Number, avg(WorkLoad) as avgWorkload
-from Rates_Course r, Student s, Course c  
-where s.NetID = 'mmd21'
-and s.Primary_Major = c.Department 
-and c.Course_Number = r.Course_Number
-group by c.Course_Number 
-order by avg(WorkLoad) asc 
+
+-- 10 students that have taken his course (random) - of a certain current year?, check! 
+select NetID, Course_Number
+from Takes_Course 
+where Course_Number = 'CS316'
+group by NetID, Course_Number
 limit 10;
+
 --classes taught by your favorite professor, check 
 select t.Professor_Name, t.Course_Number 
 from Student s, Teaches_Course t 

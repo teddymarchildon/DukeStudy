f82c1b1a01e56384fcbb04a13bc84f7798310faa
tmarchildon <tmm61@duke.edu>
2018-11-29 00:16:03 -0500

    remove unnecessary comments

--- pages/newGroup.js
+++ pages/newGroup.js
@@ -16,7 +16,7 @@ import url from 'url';
 const styles = theme => ({
   main: {
     width: 'auto',
-    display: 'block', // Fix IE 11 issue.
+    display: 'block',
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
     [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {

a9ca15b88096603b0ab92a9e865d6a78ddff61ad
tmarchildon <tmm61@duke.edu>
2018-12-04 14:20:54 -0500

    re ordered submitting of query strings

--- server.js
+++ server.js
@@ -246,13 +246,12 @@ app.prepare().then(() => {
 
     const groupID = generateGroupID();
     const course = req.body.courseID;
-    const year = req.body.courseSemester;
     let studyGroupQueryString = dbHelper.insertStudyGroupQueryString(groupID, course, year);
     let result = db.submitQueryString(res, studyGroupQueryString, false);
-
+    const year = req.body.courseSemester;
     const users = req.body.users.split(",");
     let inStudyGroupQueryString = dbHelper.insertInStudyGroupQueryString(groupID, users)
-    result = db.submitQueryString(res, inStudyGroupQueryString, false);
+    let final = db.submitQueryString(res, inStudyGroupQueryString, false);
     return;
   });
 

17223a0a9784f60ddcf04ae2a6c62a8a7f5cf251
tmarchildon <tmm61@duke.edu>
2018-12-04 09:55:43 -0500

    trim all strings in queries

--- db/query_string.js
+++ db/query_string.js
@@ -5,11 +5,11 @@ exports.createUpdateStudentQueryString = function createStudentInsertQueryString
 exports.createSelectQueryString = function createSelectQueryStringFromData(netid) {
   return `SELECT * FROM
   Student INNER JOIN Course ON Student.Favorite_Class=Course.Course_Number
-  WHERE NetID=\'${netid}\';`
+  WHERE NetID=\'${netid.trim()}\';`
 }
 
 exports.createNewUserQueryString = function createNewUserQueryString(netid, name) {
-  return `INSERT INTO Student VALUES (\'${netid}\', \'${name}\');`;
+  return `INSERT INTO Student VALUES (\'${netid.trim()}\', \'${name.trim()}\');`;
 }
 
 exports.groupsPageQueryString = function groupsPageQueryString(netid) {
@@ -17,7 +17,7 @@ exports.groupsPageQueryString = function groupsPageQueryString(netid) {
   In_Study_Group
   INNER JOIN
   Study_Group ON In_Study_Group.Group_ID=Study_Group.Group_ID
-  INNER JOIN Course ON Study_Group.Course_Number=Course.Course_Number WHERE In_Study_Group.NetID != \'${netid}\' AND In_Study_Group.Group_ID IN (SELECT Group_ID FROM In_Study_Group WHERE NetID=\'${netid}\');`
+  INNER JOIN Course ON Study_Group.Course_Number=Course.Course_Number WHERE In_Study_Group.NetID != \'${netid.trim()}\' AND In_Study_Group.Group_ID IN (SELECT Group_ID FROM In_Study_Group WHERE NetID=\'${netid.trim()}\');`
 }
 
 exports.dropDownDepartmentQueryString = function dropDownDepartmentQueryString() {
@@ -26,11 +26,11 @@ exports.dropDownDepartmentQueryString = function dropDownDepartmentQueryString()
 
 exports.dropDownCourseQueryString = function dropDownCourseQueryString(department) {
   return `SELECT Course_Number, Department, Level FROM Course WHERE
-  Department=\'${department}\';`
+  Department=\'${department.trim()}\';`
 }
 
 exports.selectTutoringQueryString = function selectTutoringQueryString(netid) {
-  return `SELECT * FROM Tutor WHERE NetID=\'${netid}\';`
+  return `SELECT * FROM Tutor WHERE NetID=\'${netid.trim()}\';`
 }
 
 exports.insertTutorQueryString = function insertTutorQueryString(data) {
@@ -40,31 +40,31 @@ exports.insertTutorQueryString = function insertTutorQueryString(data) {
 exports.allUsersQueryString = function allUsersQueryString(netid, courseID) {
   return `SELECT Takes_Course.NetID, Name, Course_Number FROM
   Takes_Course INNER JOIN Student ON Takes_Course.NetID=Student.NetID
-  WHERE Takes_Course.NetID != \'${netid}\' AND Course_Number=\'${courseID}\';`
+  WHERE Takes_Course.NetID != \'${netid.trim()}\' AND Course_Number=\'${courseID.trim()}\';`
 }
 
 exports.insertStudyGroupQueryString = function insertStudyGroupQueryString(groupID, courseID, year) {
-  return `INSERT INTO Study_Group VALUES (${groupID}, \'${courseID}\', \'${year}\');`
+  return `INSERT INTO Study_Group VALUES (${groupID.trim()}, \'${courseID.trim()}\', \'${year.trim()}\');`
 }
 
 exports.insertInStudyGroupQueryString = function insertInStudyGroupQueryString(groupID, users) {
   var string = 'INSERT INTO In_Study_Group VALUES ';
   for (user in users) {
-    string += '(' + groupID + ', \'' + users[user] + '\'),'
+    string += '(' + groupID.trim() + ', \'' + users[user].trim() + '\'),'
   }
   return string.slice(0, -1) + ';';
 }
 
 exports.removeUserFromGroupQueryString = function removeUserFromGroupQueryString(netid, groupID) {
-  return `DELETE FROM In_Study_Group WHERE Group_ID=\'${groupID}\' AND NetID=\'${netid}\';`
+  return `DELETE FROM In_Study_Group WHERE Group_ID=\'${groupID.trim()}\' AND NetID=\'${netid.trim()}\';`
 }
 
 exports.flowQueryString = function flowQueryString(netid) {
-  return `SELECT * FROM Student WHERE NetID=\'${netid}\';`
+  return `SELECT * FROM Student WHERE NetID=\'${netid.trim()}\';`
 }
 
 exports.favoriteClassQueryString = function favoriteClassQueryString(netid, courseNumber) {
-  return `UPDATE Student SET Favorite_Class=\'${courseNumber}\' WHERE NetID=\'${netid}\';`
+  return `UPDATE Student SET Favorite_Class=\'${courseNumber.trim()}\' WHERE NetID=\'${netid.trim()}\';`
 }
 
 exports.takesCourseQueryString = function takesCourseQueryString(netid, courseNumber) {

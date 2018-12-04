5c12b90ef455ac266732b8bda2dca8443c9dcfdd
tmarchildon <tmm61@duke.edu>
2018-12-03 19:41:32 -0500

    updated for production dataset

--- sql/LoadLargedb.sql
+++ sql/LoadLargedb.sql
@@ -1,34 +1,34 @@
 COPY Course(Course_Number, Department, Level, Prerequesites, Tags)
-FROM '/vagrant/Project/DukeStudy-master/CS316_Final_Project/course.csv' DELIMITER ',';
+FROM '/home/ted_marchildon/DukeStudy/CS316_Final_Project/course.csv' DELIMITER ',';
 
 COPY Professor(NetID, Name, Email, Department, Phone_Number)
-FROM '/vagrant/Project/DukeStudy-master/CS316_Final_Project/professor.csv' DELIMITER ',';
+FROM '/home/ted_marchildon/DukeStudy/CS316_Final_Project/professor.csv' DELIMITER ',';
 
 COPY Student(NetID, Name, Primary_Major, Primary_Minor, Secondary_Major, Graduation_Year, GPA, Favorite_Professor, Favorite_Class)
-FROM '/vagrant/Project/DukeStudy-master/CS316_Final_Project/student.csv' DELIMITER ',';
+FROM '/home/ted_marchildon/DukeStudy/CS316_Final_Project/student.csv' DELIMITER ',';
 
 COPY Tutor(NetID, Rate_Per_Hour, Days_Available)
-FROM '/vagrant/Project/DukeStudy-master/CS316_Final_Project/tutor.csv' DELIMITER ',';
+FROM '/home/ted_marchildon/DukeStudy/CS316_Final_Project/tutor.csv' DELIMITER ',';
 
 COPY Tutors_For(NetID, Course_Number)
-FROM '/vagrant/Project/DukeStudy-master/CS316_Final_Project/tutors_for.csv' DELIMITER ',';
+FROM '/home/ted_marchildon/DukeStudy/CS316_Final_Project/tutors_for.csv' DELIMITER ',';
 
 COPY Teaches_Course(Professor_NetID, Course_Number, Year_Semester, When_Class_Meets, Location)
-FROM '/vagrant/Project/DukeStudy-master/CS316_Final_Project/teaches_course.csv' DELIMITER ',';
+FROM '/home/ted_marchildon/DukeStudy/CS316_Final_Project/teaches_course.csv' DELIMITER ',';
 
 INSERT INTO Course_Semesters(Course_Number, Year_Semester) SELECT DISTINCT Course_Number, Year_Semester FROM Teaches_Course;
 
 COPY TAs_Course(NetID, Course_Number, Year_Semester)
-FROM '/vagrant/Project/DukeStudy-master/CS316_Final_Project/tas_course.csv' DELIMITER ',';
+FROM '/home/ted_marchildon/DukeStudy/CS316_Final_Project/tas_course.csv' DELIMITER ',';
 
 COPY Study_Group(Group_ID, Course_Number, Year_Semester)
-FROM '/vagrant/Project/DukeStudy-master/CS316_Final_Project/study_group.csv' DELIMITER ',';
+FROM '/home/ted_marchildon/DukeStudy/CS316_Final_Project/study_group.csv' DELIMITER ',';
 
 COPY Takes_Course(NetID, Course_Number, Year_Semester, Grade)
-FROM '/vagrant/Project/DukeStudy-master/CS316_Final_Project/takes_course.csv' DELIMITER ',';
+FROM '/home/ted_marchildon/DukeStudy/CS316_Final_Project/takes_course.csv' DELIMITER ',';
 
 COPY Rates_Course(NetID, Course_Number, Year_Semester, Quality_Of_Course, Quality_Of_Instruction, Difficulty, Workload)
-FROM '/vagrant/Project/DukeStudy-master/CS316_Final_Project/rates_course.csv' DELIMITER ',';
+FROM '/home/ted_marchildon/DukeStudy/CS316_Final_Project/rates_course.csv' DELIMITER ',';
 
 COPY In_Study_Group(Group_ID, NetID)
-FROM '/vagrant/Project/DukeStudy-master/CS316_Final_Project/in_study_group.csv' DELIMITER ',' CSV HEADER;
+FROM '/home/ted_marchildon/DukeStudy/CS316_Final_Project/in_study_group.csv' DELIMITER ',' CSV HEADER;

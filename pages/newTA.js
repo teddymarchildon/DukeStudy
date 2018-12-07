4abb87be42d32037df76fd6e3f40c6bbbbf68a73
tmarchildon <tmm61@duke.edu>
2018-12-07 12:13:40 -0500

    a

--- pages/newTA.js
+++ pages/newTA.js
@@ -44,7 +44,7 @@ class TAingContent extends React.Component {
     })
   }
 
-  handleRegister = event => {
+  handleRegister = async (event) => {
     let params = new URLSearchParams(this.state);
     const res = await fetch('http://35.237.162.74:3000/api/v1/ta/post', { method: 'POST', body: params });
     Router.push(`/groups?netid=${this.props.netid}`);

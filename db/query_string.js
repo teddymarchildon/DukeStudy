4a3328df107babf1fa689664b961fe84ab3697a0
tmarchildon <tmm61@duke.edu>
2018-12-06 19:48:24 -0500

    a

--- db/query_string.js
+++ db/query_string.js
@@ -117,7 +117,7 @@ exports.ratesCourseQueryString = function ratesCourseQueryString(netid, courses,
 }
 
 exports.updateRatesCourseQueryString = function updateRatesCourseQueryString(netid, course) {
-  return `UPDATE Rates_Course SET Quality_Of_Course=${course.qualityRating}, Quality_Of_Instruction-${course.qualityInstructionRating}, Difficulty=${course.difficultyRating}, Workload=${course.workloadRating} WHERE NetID=\'${netid}\' AND Course_Number=\'${course.course_number}\' AND Year_Semester=\'${course.year_semester}\';`;
+  return `UPDATE Rates_Course SET Quality_Of_Course=${course.qualityRating}, Quality_Of_Instruction=${course.qualityInstructionRating}, Difficulty=${course.difficultyRating}, Workload=${course.workloadRating} WHERE NetID=\'${netid}\' AND Course_Number=\'${course.course_number}\' AND Year_Semester=\'${course.year_semester}\';`;
 }
 
 exports.coursesQueryString = function coursesQueryString(netid) {

66a813f354a7707a76fbb72d26522c275f8a8166
tmarchildon <tmm61@duke.edu>
2018-12-07 09:16:29 -0500

    add ta

---
+++ pages/ta.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class TA extends React.Component {
+  render() {
+    return (
+      <p> This page is not yet implemented, come back later! </p>
+    )
+  }
+}
